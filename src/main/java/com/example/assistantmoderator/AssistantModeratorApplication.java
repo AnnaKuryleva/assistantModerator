d83780a4ca69c1c0867aa8e6ad1b4a4023b1aae1
Anna Kuryleva <annak12111974@gmail.com>
2025-11-17 05:04:45 +0300

    Encrypting passwords in YML

--- src/main/java/com/example/assistantmoderator/AssistantModeratorApplication.java
+++ src/main/java/com/example/assistantmoderator/AssistantModeratorApplication.java
@@ -1,13 +1,14 @@
 package com.example.assistantmoderator;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class AssistantModeratorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AssistantModeratorApplication.class, args);
+            SpringApplication.run(AssistantModeratorApplication.class, args);
+        }
     }
-
-}
